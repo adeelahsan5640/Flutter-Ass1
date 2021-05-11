f8dba2c0a8a03c4c390119970ba8c992760a9558
Adeel Ahsan <58143352+adeelahsan5640@users.noreply.github.com>
2021-05-12 04:10:11 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/adeelapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.adeelapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Text("Name: Adeel Ahsan \n Email: adeelahsan5640@gmail.com"),
+    ));
+  }
+}
